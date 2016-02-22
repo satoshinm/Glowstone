69975a4be059bed8c2c531177eb84f8eaec50526
kamcio96 <kamcio96@users.noreply.github.com>
2016-02-22 14:38:24 +0100

    Still it is only test

---
+++ src/main/java/net/glowstone/launch/GlowTweak.java
@@ -0,0 +1,35 @@
+package net.glowstone.launch;
+
+import net.minecraft.launchwrapper.ITweaker;
+import net.minecraft.launchwrapper.LaunchClassLoader;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+
+import java.io.File;
+import java.util.List;
+
+public class GlowTweak implements ITweaker {
+
+    @Override
+    public void acceptOptions(List<String> list, File file, File file1, String s) { }
+
+    @Override
+    public void injectIntoClassLoader(LaunchClassLoader launchClassLoader) {
+        MixinBootstrap.init();
+        MixinEnvironment.setCompatibilityLevel(MixinEnvironment.CompatibilityLevel.JAVA_8);
+
+        MixinEnvironment.getDefaultEnvironment()
+                .addConfiguration("mixins/mixins.glowstone.json")
+                .setSide(MixinEnvironment.Side.SERVER);
+    }
+
+    @Override
+    public String getLaunchTarget() {
+        return "net.glowstone.GlowServer";
+    }
+
+    @Override
+    public String[] getLaunchArguments() {
+        return new String[0];
+    }
+}
